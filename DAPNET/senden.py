a06f758b738585b2bb660d1be4ec5d39e6b2f60c
Philipp DL7FL <dl7fl@gmx.de>
2017-07-29 17:43:37 +0200

    daten geändert.

--- DAPNET/senden.py
+++ DAPNET/senden.py
@@ -10,23 +10,24 @@ from requests.auth import HTTPBasicAuth
 ###############################################################################
 # Konstante
 
-login = "dl7fl"  # DAPNET Benutzername
-passwd = "philipp"  # DAPNET Password
+login = ""  # DAPNET Benutzername
+passwd = ""  # DAPNET Password
 url = 'http://www.hampager.de:8080/calls'  # versenden uebers Internet
 
 
-text = "Testtext "  # Nachrichtentext bis 80 Zeichen  eingeben
-callsign = "DL7FL"  # Emfaenger Rufzeichen
+text = ""  # Nachrichtentext bis 80 Zeichen  eingeben
+callsign = ""  # Emfaenger Rufzeichen
 txgroup = "dl-all"  # Sendergruppe zB. DL-all für alle Sender in Deutschland
 
 ###############################################################################
 # Funktionen definieren
 ###############################################################################
 
-def senden(text, callsign, txgroup, login, passwd, url):
-    json_string = '''{"text": "''' + text + '''", "callSignNames": ["''' + callsign + '''"], "transmitterGroupNames": ["''' + txgroup + '''"], "emergency": false}'''
-    print(json_string)
-    response = requests.post(url, data=json_string, auth=HTTPBasicAuth(login, passwd))
-    print(response.status_code)
 
-senden(text, callsign, txgroup, login, passwd,url)
+#def senden(text, callsign, txgroup, login, passwd, url):
+json_string = '''{"text": "''' + text + '''", "callSignNames": ["''' + callsign + '''"], "transmitterGroupNames": ["''' + txgroup + '''"], "emergency": false}'''
+print(json_string)
+response = requests.post(url, data=json_string, auth=HTTPBasicAuth(login, passwd))
+print(response.status_code)
+
+#senden(text, callsign, txgroup, login, passwd,url)
