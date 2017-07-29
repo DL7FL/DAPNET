b5df5c5b627f49ce6a58f94d525e6f3dff7d5e38
Philipp DL7FL <dl7fl@gmx.de>
2017-07-29 15:58:12 +0200

    kommentare hinzugefügt und Funktion senden erstellt

--- DAPNET/senden.py
+++ DAPNET/senden.py
@@ -1,22 +1,32 @@
-###############################################################################################################
+###############################################################################
 # Philipp DL7FL mit unterstuezung von DH3RW (RWTH-AFU)
-##############################################################################################################
+###############################################################################
 
 import requests
 from requests.auth import HTTPBasicAuth
 
-##############################################################################################################
+###############################################################################
 #  Daten in Variablen Speichern
-##############################################################################################################
+###############################################################################
+# Konstante
 
-text = ""  # Nachrichtentext bis 80 Zeichen  eingeben
-callsign = ""  # Emfaenger Rufzeichen
-txgroup = ""  # Sendergruppe zB. DL-all für alle Sender in Deutschland
-login = ""  # DAPNET Benutzername
-passwd = ""  # DAPNET Password
+login = "dl7fl"  # DAPNET Benutzername
+passwd = "philipp"  # DAPNET Password
 url = 'http://www.hampager.de:8080/calls'  # versenden uebers Internet
 
-json_string = '''{"text": "''' + text + '''", "callSignNames": ["''' + callsign + '''"], "transmitterGroupNames": ["''' + txgroup + '''"], "emergency": false}'''
-print(json_string)
-response = requests.post(url, data=json_string, auth=HTTPBasicAuth(login, passwd))
-print(response.status_code)
+
+text = "Testtext "  # Nachrichtentext bis 80 Zeichen  eingeben
+callsign = "DL7FL"  # Emfaenger Rufzeichen
+txgroup = "dl-all"  # Sendergruppe zB. DL-all für alle Sender in Deutschland
+
+###############################################################################
+# Funktionen definieren
+###############################################################################
+
+def senden(text, callsign, txgroup, login, passwd, url):
+    json_string = '''{"text": "''' + text + '''", "callSignNames": ["''' + callsign + '''"], "transmitterGroupNames": ["''' + txgroup + '''"], "emergency": false}'''
+    print(json_string)
+    response = requests.post(url, data=json_string, auth=HTTPBasicAuth(login, passwd))
+    print(response.status_code)
+
+senden(text, callsign, txgroup, login, passwd,url)
