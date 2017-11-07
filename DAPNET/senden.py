c7a2de0801a128ebe69e32c188395b7f8093462e
Philipp DL7FL <30368381+DL7FL@users.noreply.github.com>
2017-11-07 21:08:09 +0100

    mehrere Rufzeichen

--- DAPNET/senden.py
+++ DAPNET/senden.py
@@ -8,25 +8,35 @@ from requests.auth import HTTPBasicAuth
 ###############################################################################
 #  Daten in Variablen Speichern
 ###############################################################################
+
 # Konstante
 
 login = ""  # DAPNET Benutzername
 passwd = ""  # DAPNET Password
 url = 'http://www.hampager.de:8080/calls'  # versenden uebers Internet
 
+text = ""  # Nachrichtentext bis 80 Zeichen  eingeben
+rufzeichen = ["DL7FL","DL7FL"]  #  # eins oder mehrere Emfaenger Rufzeichen
+txgroup = "dl-he"  # Sendergruppe zB. DL-all für alle Sender in Deutschland
 
-text = "text"  # Nachrichtentext bis 80 Zeichen  eingeben
-callsign = ""  # Emfaenger Rufzeichen
-txgroup = "dl-he" # Sendergruppe zB. DL-all fuer alle Sender in Deutschland
 
 ###############################################################################
 # Funktionen definieren
 ###############################################################################
 
 def senden(text, callsign, txgroup, login, passwd, url):
-    json_string = '''{"text": "''' + text + '''", "callSignNames": ["''' + callsign + '''"], "transmitterGroupNames": ["''' + txgroup + '''"], "emergency": false}'''
-    print(json_string)
-    response = requests.post(url, data=json_string, auth=HTTPBasicAuth(login, passwd))
-    print(response.status_code)
+	# print(callsign)
+	json_string = '''{"text": "''' + text + '''", "callSignNames": ["''' + callsign + '''"], "transmitterGroupNames": ["''' + txgroup + '''"], "emergency": false}'''
+	# print(json_string)
+	response = requests.post(url, data=json_string, auth=HTTPBasicAuth(login, passwd))
+	print(response.status_code)
 
-senden(text, callsign, txgroup, login, passwd,url)
+
+def Rufzeichen_vereinzeln(rufzeichen):  #  Rufzeichen vereinzelt und ruft mit jedem Rufzeichen die Senden Funktion auf.
+	for callsign in rufzeichen:
+		# print(callsign)
+
+		senden(text, callsign, txgroup, login, passwd, url)
+
+
+Rufzeichen_vereinzeln(rufzeichen)
