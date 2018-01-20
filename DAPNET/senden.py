0e05b64111b56e92756f073815f10b63a59aafc7
Philipp DL7FL <30368381+DL7FL@users.noreply.github.com>
2018-01-20 11:04:51 +0100

    ausgelagerte Funktionen gelöscht.

--- DAPNET/senden.py
+++ DAPNET/senden.py
@@ -27,32 +27,12 @@ passwd = os.getenv('DAPNET_Passwort')  #  DAPNET Passwort aus Umgebungsvariablen
 url = 'http://www.hampager.de:8080/calls'  #  versenden uebers Internet Variable
 
 text = "test cccffm"  #  Nachrichte ntext bis 80 Zeichen  eingeben
-callsign_list = ["dl7fl","DH2fg"]  # eins oder mehrere Emfaenger Rufzeichen
+callsign_list = ["dl7fl"]  # eins oder mehrere Emfaenger Rufzeichen
 txgroup = "dl-he"  #  Sendergruppe zB. DL-all für alle Sender in Deutschland
 
-###############################################################################
-# Funktionen definieren
-###############################################################################
-'''
-def send(text, callsign, login, passwd, url,txgroup="dl-he"): # mit json modul machen
-	""" Sendet JASON-String zur Funkruf senden."""
-
-	json_string =json.dumps({"text": text, "callSignNames": callsign, "transmitterGroupNames": [txgroup], "emergency": False})
-	import pprint; pprint.pprint(json_string)
-	response = requests.post(url, data=json_string, auth=HTTPBasicAuth(login, passwd)) # Exception handling einbauen
-	return response.status_code
-
-
-def single_callsign(callsign_list): #  Rufzeichen vereinzelt und ruft mit jedem Rufzeichen die Send Funktion auf.
-	"""Zerlegt die callsign_list in einzelne callsign"""
-	for callsign in callsign_list:
-		send(text, callsign, login, passwd, url)
-	return
-
-'''
 ##############################################################################
 # Hauptprogramm
 ##############################################################################
 
-#single_callsign(callsign_list)
+
 dapnet.send(text, callsign_list, login, passwd, url)
