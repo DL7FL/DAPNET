82fdccdec8e2f6efa67cdeb8654dab9964d4111c
DL7FL <DL7FL@darc.de>
2018-07-29 21:25:13 +0200

    emergency als Schlüsselwortagument mit Standartwert hinzugefügt.

--- DAPNET/dapnet.py
+++ DAPNET/dapnet.py
@@ -6,10 +6,10 @@ import json
 # Funktionen definieren
 ###############################################################################
 
-def send(text, callsign, login, passwd, url,txgroup="dl-he"): # mit json modul machen
+def send(text, callsign, login, passwd, url,txgroup="dl-he", emergency = False): # mit json modul machen
 	""" Sendet JASON-String zur Funkruf senden."""
 
-	json_string =json.dumps({"text": text, "callSignNames": callsign, "transmitterGroupNames": [txgroup], "emergency": False})
+	json_string =json.dumps({"text": text, "callSignNames": callsign, "transmitterGroupNames": [txgroup], "emergency": emergency})
 	import pprint; pprint.pprint(json_string)
 	response = requests.post(url, data=json_string, auth=HTTPBasicAuth(login, passwd)) # Exception handling einbauen
 	return response.status_code
--- DAPNET/senden_gui.py
+++ DAPNET/senden_gui.py
@@ -8,22 +8,25 @@ import logging # -> Logging vom Fehlermeldenung
 import sys
 from tkinter import *
 
-
+# Logging
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s;%(levelname)s;%(message)s")
 logger = logging.getLogger(sys.argv[0])
 
-###############################################################################
-#  Daten in Variablen Speichern
-###############################################################################
+# GUI Funktionen
+
 # Die folgende Funktion soll ausgeführt werden, wenn
-# der Benutzer den Button Klick mich anklickt
-def button_action():
-    entry_text = eingabefeld.get()
+# der Benutzer den Button Senden anklickt
+def button_senden():
+    entry_text = eingabefeld_nachricht.get()
     if (entry_text == ""):
-        welcome_label.config(text="Gib zuerst einen Namen ein.")
+        status_label.config(text="Gib zuerst eine Nachricht ein.")
     else:
-        entry_text = "Welcome " + entry_text + "!"
-        welcome_label.config(text=entry_text)
+        text_nachricht = entry_text + "test"
+        status_label.config(text=text_nachricht)
+
+ ###############################################################################
+ #  Daten in Variablen Speichern
+ ###############################################################################
 
 # Konstante
 
@@ -32,7 +35,7 @@ passwd = os.getenv('DAPNET_Passwort')  #  DAPNET Passwort aus Umgebungsvariablen
 
 url = 'http://www.hampager.de:8080/calls'  #  versenden uebers Internet Variable
 
-text = "Dapnet test DL7FL"  #  Nachrichte ntext bis 80 Zeichen  eingebe
+#text = "Dapnet test DL7FL"  #  Nachrichte ntext bis 80 Zeichen  eingebe
 callsign_list = ["dl7fl"]  # eins oder mehrere Emfaenger Rufzeichen DL4FLY
 txgroup = "dl-he"  #  Sendergruppe zB. DL-all für alle Sender in Deutschland
 
@@ -42,32 +45,35 @@ txgroup = "dl-he"  #  Sendergruppe zB. DL-all für alle Sender in Deutschland
 fenster = Tk()
 fenster.title("DAPNET Funkruf")
 
-# Anweisungs-Label
-my_label = Label(fenster, text="Gib deinen Namen ein: ")
+# Nachricht-Label
+nachricht_label = Label(fenster, text="Nachricht eingeben: ")
 
-# In diesem Label wird nach dem Klick auf den Button der Benutzer
-# mit seinem eingegebenen Namen begrüsst.
-welcome_label = Label(fenster)
+# In diesem Label wird nach dem Klick auf den Button der Status ausgegeben
+status_label = Label(fenster)
 
-# Hier kann der Benutzer eine Eingabe machen
-eingabefeld = Entry(fenster, bd=5, width=40)
+# Eingabefeld für Nachricht
+eingabefeld_nachricht = Entry(fenster, bd=5, width=80)
 
-welcom_button = Button(fenster, text="Klick me", command=button_action)
+# Sende Button ruft Funktion button_senden auf
+senden_button = Button(fenster, text="Senden", command=button_senden())
+# Button zum Programm beenden
 exit_button = Button(fenster, text="Beenden", command=fenster.quit)
 
 
-# Nun fügen wir die Komponenten unserem Fenster hinzu
-my_label.grid(row = 0, column = 0)
-eingabefeld.grid(row = 0, column = 1)
-welcom_button.grid(row = 1, column = 0)
+# Komponenten Anordnungim Fenster
+nachricht_label.grid(row = 0, column = 0)
+eingabefeld_nachricht.grid(row = 0, column = 1)
+
+senden_button.grid(row = 1, column = 0)
 exit_button.grid(row = 1, column = 1)
-welcome_label.grid(row = 2, column = 0, columnspan = 2)
+
+status_label.grid(row = 2, column = 0, columnspan = 2)
 
 ##############################################################################
 # Hauptprogramm
 ##############################################################################
 
 
-dapnet.send(text, callsign_list, login, passwd, url, txgroup)
+#dapnet.send(text, callsign_list, login, passwd, url, txgroup)
 
 mainloop()
