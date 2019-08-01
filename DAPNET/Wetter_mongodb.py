e7d6808310b68831021f689323e387ebf91ed224
DL7FL <DL7FL@darc.de>
2019-08-01 10:21:37 +0200

    Pandas Teil entfernt.

--- DAPNET/Wetter_mongodb.py
+++ DAPNET/Wetter_mongodb.py
@@ -13,7 +13,6 @@ unwetterjson = requests.get(url)
 unwetterwarnunglist = json.loads(unwetterjson.text)
 
 print(type(unwetterwarnunglist))
-print(type(unwetterwarnunglist[0]))
 
 for unwetterwarnung in unwetterwarnunglist:
     post_id = posts.insert_one(unwetterwarnung).inserted_id
@@ -34,33 +33,4 @@ for doc in posts.find():
 
 len({i['sent'] for i in posts.find()})
 
-# Mongo zu Panda df
-data = pd.DataFrame(list(posts.find()))
-
-print(posts.find_one('id'))
-
-data[(data['sent'] > '2018-12-30T19:55:26+01:00') & (data['sent'] < '2019-03-25T16:40:38+01:00')]
-data['sent'] = pd.to_datetime(data['sent'])
-
-len(data['info'][0][0]['area'])
-x = data['info']
-data['anzahl'] = x.apply(lambda x: len(x[0]['area']))
-
-data['anzahl']
-
-len(data['anzahl'])
-data.set_index('sent',inplace=True)
-data.anzahl.plot(legend=True)
-data.hist()
-
-# Pie Chart
-var= data.groupby(['anzahl']).sum().stack()
-temp = var.unstack()
-type(temp)
-x_list = temp['anzahl']
-label_list = temp.index
-pyplot.axis("equal") # Kreisdiagramm rund gestaltet (sonst Standard: oval!)
-pyplot.pie(x_list, labels=label_list, autopct="%1.1f%%")
-pyplot.title('Aufteilung alle Mitarbeiter auf die Standorte nach Funktion')
-pyplot.show()
 
